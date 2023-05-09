398d5aa8f23749b39ef1af5a58d615063f5645b7
LHKN <79393909+LHKN@users.noreply.github.com>
2023-05-09 16:14:05 +0700

    chore: resolve merge error

--- app/src/main/java/com/example/fit_20clc_hcmus_android_final_project/DetailedPlan.java
+++ app/src/main/java/com/example/fit_20clc_hcmus_android_final_project/DetailedPlan.java
@@ -2,6 +2,7 @@ package com.example.fit_20clc_hcmus_android_final_project;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
